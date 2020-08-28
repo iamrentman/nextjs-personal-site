95259414a0a1bc077cf89d6a73fe6f836512de43
Justin Rent <jrentdesign@gmail.com>
2020-08-27 22:19:00 -0400

    Moving data file to new directory.

--- pages/resume.js
+++ pages/resume.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout/layout'
 import utilStyles from '../global/utils.module.scss'
 import Date from '../components/date/date'
-import data from '../content/resdata.json'
+import data from '../data/resdata.json'
 import NewThing from '../components/newthing/newthing'
 
 export default function Resume() {
