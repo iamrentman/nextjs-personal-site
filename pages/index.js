1d92c0daf7a81fef5d9b1faa603efeb9e2352e85
Justin Rent <jrentdesign@gmail.com>
2020-10-12 08:29:51 -0400

    Experience content updates

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allContentData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.intro}>
-        <p>I'm excited to be reworking my personal website with Next.js! Things are pretty basic right now, but I've got big plans. For the time being you can read a bit <Link href="about">about me</Link>, learn more about my <Link href="resume">professional experience</Link>, or read some updates below.</p>
+        <p>I'm excited to be reworking my personal website with Next.js! Things are pretty basic right now, but I've got big plans. For the time being you can read a bit <Link href="about">about me</Link>, learn more about my <Link href="resume">professional background</Link>, or read some updates below.</p>
         <p>If you'd like to connect, you can also find me on <a href="https://www.linkedin.com/in/jrentdesign/">LinkedIn</a>, <a href="https://www.github.com/iamrentman">GitHub</a>, <a href="https://www.codepen.io/iamrentman">CodePen</a>, <a href="https://www.dribbble.com/iamrentman">Dribbble</a>, <a href="https://www.twitter.com/iamrentman">Twitter</a>, and <a href="https://www.instagram.com/iamrentman">Instagram</a>.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
